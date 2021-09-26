b98b3119a7315f37dfc97a09a50ad4436b8b5b0b
efburgosr <efburgosr@unal.edu.co>
2021-09-25 20:01:31 -0500

    Adicionado componente menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import MenuComponente from './components/MenuComponente';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+        <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+        <div className="navbar-nav">
+          <div className="nav-item text-nowrap">
+            <a className="nav-link px-3" href="#">Sign out</a>
+          </div>
+        </div>
       </header>
+      <div className="container-fluid">
+        <div className="row">
+          <MenuComponente />
+        </div>
+      </div>
     </div>
   );
 }
