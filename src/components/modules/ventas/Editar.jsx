526e70381dddb6167eccb0717b1be04bc8ac96a8
Cristian Parra <cjparraq@gmail.com>
2021-10-02 01:27:36 -0500

    Adición Pantallas CRUD Ventas

---
+++ src/components/modules/ventas/Editar.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import { Form, Button, Row, Col, Table } from "react-bootstrap"
+
+const Editar = () => {
+    return (
+        <div className="contenedor">
+            <hr />
+            <h4> (Editar) Detalles de facturación</h4>
+            <hr />
+
+            <Form.Text className="text-muted">
+                Los campos marcados con (*) son obligatorios.
+            </Form.Text>
+
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">ID Venta</Form.Label>
+                    <Col sm="10">
+                        <Form.Control plaintext readOnly defaultValue="0001" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Tipo DNI*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="TI - Tarjeta de Identidad" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Nombre Cliente*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="Victoria Valencia" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Correo electrónico*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="vvalencia@unal.edu.co" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Telefono*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="3005066745" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Dirección*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="Avenida 80 # 45-60" />
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">Ciudad*</Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="name" placeholder="Medellín" />
+                    </Col>
+                </Form.Group>
+
+                <hr />
+                <h4> (Editar) Productos de la venta</h4>
+                <hr />
+                <div>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Producto</th>
+                                <th>Cantidad</th>
+                                <th>Valor Unitario</th>
+                                <th>Valor Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><Form.Control type="name" placeholder="Chaqueta" /></td>
+                                <td><Form.Control type="name" placeholder="1" /></td>
+                                <td><Form.Control type="name" placeholder="$ 85.000" /></td>
+                                <td><Form.Control type="name" placeholder="$ 85.000" /></td>
+                            </tr>
+                            <tr>
+                                <td><Form.Control type="name" placeholder="Camisa" /></td>
+                                <td><Form.Control type="name" placeholder="3" /></td>
+                                <td><Form.Control type="name" placeholder="$ 25.000" /></td>
+                                <td><Form.Control type="name" placeholder="$ 75.000" /></td>
+                            </tr>
+                            <tr>
+                                <td><Form.Control type="name" placeholder="Jean" /></td>
+                                <td><Form.Control type="name" placeholder="2" /></td>
+                                <td><Form.Control type="name" placeholder="$ 95.000" /></td>
+                                <td><Form.Control type="name" placeholder="$ 190.000" /></td>
+                            </tr>
+                            <tr>
+                                <td colSpan="3">TOTAL FACTURA</td>
+                                <td><Form.Control type="name" placeholder="$ 350.000" /></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                <Button variant="primary" type="submit">Guardar Cambios</Button>
+                <hr />
+            </Form>
+        </div>
+    )
+}
+
+export default Editar
--- src/components/modules/ventas/Ventas.jsx
+++ src/components/modules/ventas/Ventas.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button} from "react-bootstrap"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Crear from './Crear';
+import Listar from './Listar';
+import Editar from './Editar';
 
 const Ventas = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <div className="container">
+                <hr />
+                <h1>Ventas</h1>
+                <hr />
+                <Link to="/ventas/crear"><Button href="crear" variant="secondary">Crear</Button></Link>
+                <Link to="/ventas/listar"><Button href="listar" variant="secondary">Listar</Button></Link>
+                <Switch>
+                    <Route path="/ventas/crear">
+                        <Crear />
+                    </Route>
+                    <Route path="/ventas/listar">
+                        <Listar />
+                    </Route>
+                    <Route path="/ventas/editar">
+                        <Editar />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
